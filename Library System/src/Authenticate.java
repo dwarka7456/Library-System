f713ce21f06bf87fe0c0c187dd2824c8a00f4494
Yadav <Yadav@Dwarka>
2017-10-04 22:54:23 -0400

    init

---
+++ Library System/src/Authenticate.java
@@ -0,0 +1,12 @@
+
+public class Authenticate {
+
+	private String userName;
+	private String password;
+	
+	
+	public void Authenticate () {
+		System.out.println("Dwarka is Authenti...");
+	}
+	
+}
